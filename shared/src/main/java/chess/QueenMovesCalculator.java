b06722a8e85d6c5c2c161442b795bcedb0c2c7dc
Lorin <lrmt13@gmail.com>
2024-01-23 12:26:50 -0700

    passed QueenMoveTests

--- shared/src/main/java/chess/QueenMovesCalculator.java
+++ shared/src/main/java/chess/QueenMovesCalculator.java
@@ -9,6 +9,86 @@ public class QueenMovesCalculator extends PieceMovesCalculator{
 
     @Override
     public Collection<ChessMove> pieceMoves() {
-        return null;
+        // up
+        for (int i = position.getRow()+1; i<9; i++) {
+            ChessPosition newPosition = new ChessPosition(i, position.getColumn());
+            if (IsAvailableSquare(newPosition)) {
+                AddValidMove(newPosition);
+            }
+            if (IsOccupied(newPosition)) {
+                break;
+            }
+        }
+        // down
+        for (int i = position.getRow()-1; i>0; i--) {
+            ChessPosition newPosition = new ChessPosition(i, position.getColumn());
+            if (IsAvailableSquare(newPosition)) {
+                AddValidMove(newPosition);
+            }
+            if (IsOccupied(newPosition)) {
+                break;
+            }
+        }
+        // left
+        for (int i = position.getColumn()-1; i>0; i--) {
+            ChessPosition newPosition = new ChessPosition(position.getRow(), i);
+            if (IsAvailableSquare(newPosition)) {
+                AddValidMove(newPosition);
+            }
+            if (IsOccupied(newPosition)) {
+                break;
+            }
+        }
+        // right
+        for (int i = position.getColumn()+1; i<9; i++) {
+            ChessPosition newPosition = new ChessPosition(position.getRow(), i);
+            if (IsAvailableSquare(newPosition)) {
+                AddValidMove(newPosition);
+            }
+            if (IsOccupied(newPosition)) {
+                break;
+            }
+        }
+        // up left
+        for (int i = position.getRow()+1, j = position.getColumn()-1; i<9 && j>0; i++, j--) {
+            ChessPosition newPosition = new ChessPosition(i,j);
+            if (IsAvailableSquare(newPosition)) {
+                AddValidMove(newPosition);
+            }
+            if (IsOccupied(newPosition)) {
+                break;
+            }
+        }
+        // down left
+        for (int i = position.getRow()-1, j = position.getColumn()-1; i>0 && j>0; i--, j--) {
+            ChessPosition newPosition = new ChessPosition(i,j);
+            if (IsAvailableSquare(newPosition)) {
+                AddValidMove(newPosition);
+            }
+            if (IsOccupied(newPosition)) {
+                break;
+            }
+        }
+        // up right
+        for (int i = position.getRow()+1, j = position.getColumn()+1; i<9 && j<9; i++, j++) {
+            ChessPosition newPosition = new ChessPosition(i,j);
+            if (IsAvailableSquare(newPosition)) {
+                AddValidMove(newPosition);
+            }
+            if (IsOccupied(newPosition)) {
+                break;
+            }
+        }
+        // down right
+        for (int i = position.getRow()-1, j = position.getColumn()+1; i>0 && j<9; i--, j++) {
+            ChessPosition newPosition = new ChessPosition(i,j);
+            if (IsAvailableSquare(newPosition)) {
+                AddValidMove(newPosition);
+            }
+            if (IsOccupied(newPosition)) {
+                break;
+            }
+        }
+        return validMoves;
     }
 }
