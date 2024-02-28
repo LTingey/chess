4563c6661b29facb1edf64863513e749090b4c79
Lorin <lrmt13@gmail.com>
2024-02-27 20:23:25 -0700

    register handlers

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,8 @@
 package server;
 
+import handlers.ClearHandler;
+import handlers.GameHandler;
+import handlers.UserHandler;
 import spark.*;
 
 public class Server {
@@ -9,7 +12,13 @@ public class Server {
 
         Spark.staticFiles.location("web");
 
-        // Register your endpoints and handle exceptions here.
+        Spark.delete("/db", ClearHandler::clear);
+        Spark.post("/user", UserHandler::register);
+        Spark.post("/sesssion", UserHandler::login);
+        Spark.delete("/session", UserHandler::logout);
+        Spark.get("/game", GameHandler::listGames);
+        Spark.post("/game", GameHandler::createGame);
+        Spark.put("/game", GameHandler::joinGame);
 
         Spark.awaitInitialization();
         return Spark.port();
