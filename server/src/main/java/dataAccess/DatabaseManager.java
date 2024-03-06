a1371729a8b7a91f54493b412cea691416a4ffc2
Lorin <lrmt13@gmail.com>
2024-03-06 14:12:56 -0700

    phase 4 starter code

--- server/src/main/java/dataAccess/DatabaseManager.java
+++ server/src/main/java/dataAccess/DatabaseManager.java
@@ -1,76 +1,131 @@
 package dataAccess;
 
-import model.AuthData;
-import model.GameData;
-import model.UserData;
-
-import java.util.HashSet;
+import java.sql.*;
+import java.util.Properties;
 
 public class DatabaseManager {
-    HashSet<UserData> userDatabase = new HashSet<>();
-    HashSet<AuthData> authDatabase = new HashSet<>();
-    HashSet<GameData> gameDatabase = new HashSet<>();
+    private static final String databaseName;
+    private static final String user;
+    private static final String password;
+    private static final String connectionUrl;
 
-    public UserData findUser(String username) {
-        for (UserData user : userDatabase) {
-            if (user.username().equals(username)) {
-                return user;
+    /*
+     * Load the database information for the db.properties file.
+     */
+    static {
+        try {
+            try (var propStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("db.properties")) {
+                if (propStream == null) throw new Exception("Unable to load db.properties");
+                Properties props = new Properties();
+                props.load(propStream);
+                databaseName = props.getProperty("db.name");
+                user = props.getProperty("db.user");
+                password = props.getProperty("db.password");
+
+                var host = props.getProperty("db.host");
+                var port = Integer.parseInt(props.getProperty("db.port"));
+                connectionUrl = String.format("jdbc:mysql://%s:%d", host, port);
             }
+        } catch (Exception ex) {
+            throw new RuntimeException("unable to process db.properties. " + ex.getMessage());
         }
-        return null;
     }
 
-    public void addUser(UserData newUser) {
-        userDatabase.add(newUser);
+    /**
+     * Creates the database if it does not already exist.
+     */
+    public static void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        DatabaseManager.createUserTable();
+        DatabaseManager.createAuthTable();
+        DatabaseManager.createGameTable();
     }
 
-    public AuthData findAuth(String authToken) {
-        for (AuthData authData : authDatabase) {
-            if (authData.authToken().equals(authToken)) {
-                return authData;
+    static void createDatabase() throws DataAccessException {
+        try {
+            var statement = "CREATE DATABASE IF NOT EXISTS " + databaseName;
+            var conn = DriverManager.getConnection(connectionUrl, user, password);
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.executeUpdate();
             }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
         }
-        return null;
     }
 
-    public void addAuth(AuthData newAuth) {
-        authDatabase.add(newAuth);
-    }
-
-    public void removeAuth(AuthData authData) {
-        authDatabase.remove(authData);
-    }
-
-    public GameData findGame(int gameID) {
-        for (GameData game : gameDatabase) {
-            if (game.gameID() == gameID) {
-                return game;
+    static void createUserTable() throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = """
+                    CREATE TABLE IF NOT EXISTS users (
+                        id INT NOT NULL AUTO_INCREMENT,
+                        username VARCHAR(255) NOT NULL,
+                        password VARCHAR(255) NOT NULL,
+                        email VARCHAR(255) NOT NULL,
+                        PRIMARY KEY (id)
+                    )""";
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.executeUpdate();
             }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
         }
-        return null;
     }
 
-    public void addGame(GameData newGame) {
-        gameDatabase.add(newGame);
+    static void createAuthTable() throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = """
+                    CREATE TABLE IF NOT EXISTS auths (
+                        id INT NOT NULL AUTO_INCREMENT,
+                        username VARCHAR(255) NOT NULL,
+                        authToken VARCHAR(255) NOT NULL,
+                        PRIMARY KEY (id)
+                    )""";
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 
-    public void removeGame(GameData game) {
-        gameDatabase.remove(game);
+    static void createGameTable() throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = """
+                    CREATE TABLE IF NOT EXISTS games (
+                        id INT NOT NULL AUTO_INCREMENT,
+                        whitUsername VARCHAR(255),
+                        blackUsername VARCHAR(255),
+                        gameName VARCHAR(255) NOT NULL,
+                        game longtext NOT NULL
+                        PRIMARY KEY (id)
+                    )""";
+            try (var preparedStatement = conn.prepareStatement(statement)) {
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 
-    public HashSet<GameData> allGames() {
-        return gameDatabase;
-    }
-
-    public void clearUsers() {
-        userDatabase.clear();
-    }
-
-    public void clearAuths() {
-        authDatabase.clear();
-    }
-
-    public void clearGames() {
-        gameDatabase.clear();
+    /**
+     * Creates a connection to the database and sets the catalog based upon the
+     * properties specified in db.properties. Connections to the database should
+     * be short-lived, and you must close the connection when you are done with it.
+     * The easiest way to do that is with a try-with-resource block.
+     * <br/>
+     * <code>
+     * try (var conn = DbInfo.getConnection(databaseName)) {
+     * // execute SQL statements.
+     * }
+     * </code>
+     */
+    public static Connection getConnection() throws DataAccessException {
+        try {
+            var conn = DriverManager.getConnection(connectionUrl, user, password);
+            conn.setCatalog(databaseName);
+            return conn;
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 }
