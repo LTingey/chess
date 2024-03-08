cf32e71cdf0b9049bf4567942a793751bdd04a78
Lorin <lrmt13@gmail.com>
2024-03-07 19:42:30 -0700

    initial implementation of SQLGameDAO

--- server/src/main/java/dataAccess/sql/SQLGameDAO.java
+++ server/src/main/java/dataAccess/sql/SQLGameDAO.java
@@ -1,34 +1,96 @@
 package dataAccess.sql;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
 import dataAccess.DataAccessException;
 import dataAccess.DatabaseManager;
 import dataAccess.GameDAO;
 import model.GameData;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashSet;
 
-public class SQLGameDAO implements GameDAO {
+public class SQLGameDAO extends SQLDAO implements GameDAO {
     public SQLGameDAO() throws DataAccessException {
         DatabaseManager.configureDatabase();
     }
 
-    public int createGame(String gameName) {
-        return 0;
+    public int createGame(String gameName) throws DataAccessException {
+        return addGame(gameName, new ChessGame());
     }
 
-    public GameData getGame(int gameID) {
+    public GameData getGame(int gameID) throws DataAccessException {
+        String statement = "SELECT id, whiteUsername, blackUsername, gameName, game FROM games WHERE id=?";
+        try (ResultSet result = getDataByID(statement, gameID)) {
+            if (result.next()) {
+                return readGame(result);
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
         return null;
     }
 
-    public HashSet<GameData> listGames() {
-        return null;
+    public HashSet<GameData> listGames() throws DataAccessException {
+        HashSet<GameData> games = new HashSet<>();
+        try (var conn = DatabaseManager.getConnection()) {
+            String statement = "SELECT id, whiteUsername, blackUsername, gameName, game FROM games";
+            try (PreparedStatement preparedStatement = conn.prepareStatement(statement)) {
+                try (ResultSet result = preparedStatement.executeQuery()) {
+                    while (result.next()) {
+                        games.add(readGame(result));
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+        return games;
     }
 
-    public void updateGame(GameData game) {
-
+    public void updateGame(GameData game) throws DataAccessException {
+        var statement = "DELETE FROM games WHERE id=?";
+        executeUpdate(statement, game.gameID());
+        addGame(game.gameName(), game.game());
     }
 
-    public void clear() {
+    public void clear() throws DataAccessException {
+        var statement = "TRUNCATE games";
+        executeUpdate(statement);
+    }
 
+    private int addGame(String gameName, ChessGame game) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "INSERT INTO games (gameName, game) VALUES(?, ?)";
+            try (PreparedStatement preparedStatement = conn.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS)) {
+                String jsonGame = new Gson().toJson(game);
+                preparedStatement.setString(1, gameName);
+                preparedStatement.setString(2, jsonGame);
+
+                preparedStatement.executeUpdate();
+
+                ResultSet result = preparedStatement.getGeneratedKeys();
+                int id = 0;
+                if (result.next()) {
+                    id = result.getInt(1);
+                }
+                return id;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    private GameData readGame(ResultSet result) throws SQLException {
+        var id = result.getInt("id");
+        var whiteUsername = result.getString("whiteUsername");
+        var blackUsername = result.getString("blackUsername");
+        var gameName = result.getString("gameName");
+        var jsonGame = result.getString("game");
+        ChessGame game = new Gson().fromJson(jsonGame, ChessGame.class);
+        return new GameData(id, whiteUsername, blackUsername, gameName, game);
     }
 }
