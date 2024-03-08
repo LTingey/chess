0a7761d54db3ca0a2de4e3820b41f74e5d7643e3
Lorin <lrmt13@gmail.com>
2024-03-08 13:39:11 -0700

    fix join game bug and pass all service tests

--- server/src/main/java/dataAccess/sql/SQLGameDAO.java
+++ server/src/main/java/dataAccess/sql/SQLGameDAO.java
@@ -16,7 +16,7 @@ import java.util.HashSet;
 public class SQLGameDAO extends SQLDAO implements GameDAO {
 
     public int createGame(String gameName) throws DataAccessException {
-        return addGame(gameName, new ChessGame());
+        return addGame(null, null, gameName, new ChessGame());
     }
 
     public GameData getGame(int gameID) throws DataAccessException {
@@ -56,7 +56,7 @@ public class SQLGameDAO extends SQLDAO implements GameDAO {
     public int updateGame(GameData game) throws DataAccessException {
         var statement = "DELETE FROM games WHERE id=?";
         executeUpdate(statement, game.gameID());
-        int newID = addGame(game.gameName(), game.game());
+        int newID = addGame(game.whiteUsername(), game.blackUsername(), game.gameName(), game.game());
         return newID;
     }
 
@@ -65,13 +65,15 @@ public class SQLGameDAO extends SQLDAO implements GameDAO {
         executeUpdate(statement);
     }
 
-    private int addGame(String gameName, ChessGame game) throws DataAccessException {
+    private int addGame(String whiteUsername, String blackUsername, String gameName, ChessGame game) throws DataAccessException {
         try (var conn = DatabaseManager.getConnection()) {
-            var statement = "INSERT INTO games (gameName, game) VALUES(?, ?)";
+            var statement = "INSERT INTO games (whiteUsername, blackUsername, gameName, game) VALUES(?, ?, ?, ?)";
             try (PreparedStatement preparedStatement = conn.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS)) {
                 String jsonGame = new Gson().toJson(game);
-                preparedStatement.setString(1, gameName);
-                preparedStatement.setString(2, jsonGame);
+                preparedStatement.setString(1, whiteUsername);
+                preparedStatement.setString(2, blackUsername);
+                preparedStatement.setString(3, gameName);
+                preparedStatement.setString(4, jsonGame);
 
                 preparedStatement.executeUpdate();
 
