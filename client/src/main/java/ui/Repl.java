ce9c2145c96d11aee8716d65c1df47982616cd00
Lorin <lrmt13@gmail.com>
2024-03-23 18:37:27 -0600

    initial implementation

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,5 +1,9 @@
 package ui;
 
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
 public class Repl {
     private ChessClient client;
 
@@ -8,6 +12,26 @@ public class Repl {
     }
 
     public void run() {
+        System.out.println("Welcome to CS240 Chess. Type Help to get started.");
+        Scanner scanner = new Scanner(System.in);
 
+        String output = "";
+        while (!output.equals("Quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                output = client.evalInput(line);
+                System.out.print(SET_TEXT_COLOR_BLUE);
+            } catch (Throwable e) {
+                String message = e.toString();
+                System.out.print(message);
+            }
+        }
+    }
+
+    private void printPrompt() {
+        String state = client.getState().toString();
+        System.out.print(SET_TEXT_COLOR_DARK_GREY + "\n[" + state + "] >>> " + SET_TEXT_COLOR_RED);
     }
 }
