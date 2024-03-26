e7141a19d436c0e55449dc08bd6c2e16b1cc27ad
Lorin <lrmt13@gmail.com>
2024-03-26 15:39:36 -0600

    format output; write specific Exception class

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -12,19 +12,19 @@ public class Repl {
     }
 
     public void run() {
-        System.out.println("Welcome to CS240 Chess. Type Help to get started.");
+        System.out.println(SET_TEXT_COLOR_BLUE + "Welcome to CS240 Chess. Type Help to get started.");
         Scanner scanner = new Scanner(System.in);
 
         String output = "";
-        while (!output.equals("Quit")) {
+        while (!output.equals("Goodbye!")) {
             printPrompt();
             String line = scanner.nextLine();
 
             try {
                 output = client.evalInput(line);
-                System.out.print(SET_TEXT_COLOR_BLUE);
-            } catch (Throwable e) {
-                String message = e.toString();
+                System.out.print(output + RESET_TEXT_COLOR + RESET_BG_COLOR);
+            } catch (ResponseException e) {
+                String message = SET_TEXT_COLOR_RED + e.getMessage() + "\n";
                 System.out.print(message);
             }
         }
@@ -32,6 +32,6 @@ public class Repl {
 
     private void printPrompt() {
         String state = client.getState().toString();
-        System.out.print(SET_TEXT_COLOR_DARK_GREY + "\n[" + state + "] >>> " + SET_TEXT_COLOR_RED);
+        System.out.print(SET_TEXT_COLOR_LIGHT_GREY + "\n[" + state + "] >>> " + SET_TEXT_COLOR_BLUE);
     }
 }
--- server/src/main/java/dataAccess/DataAccessException.java
+++ server/src/main/java/dataAccess/DataAccessException.java
@@ -3,7 +3,7 @@ package dataAccess;
 /**
  * Indicates there was an error connecting to the database
  */
-public class DataAccessException extends Exception{
+public class DataAccessException extends Exception {
     public DataAccessException(String message) {
         super(message);
     }
